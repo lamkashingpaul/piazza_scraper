62ae173869992edb6129edf35d943dccb9467297
Paul Lam <lamkashingpaul@gmail.com>
2021-10-18 21:05:37 +0800

    Fix Invalid Filename Issue

--- main.py
+++ main.py
@@ -4,54 +4,69 @@ import os
 import re
 import requests
 
-username = input('Email: ')
-password = getpass.getpass('Password: ')
 
-payload = {
-    'from': '/signup',
-    'email': username,
-    'password': password,
-}
+def flatten_string(s):
+    for c in '\\/:*?"<>|':
+        s = s.replace(c, '_')
+    return s.lstrip()
 
-login_request_url = 'https://piazza.com/class'
-resource = input('Resource url: ')
 
-s = requests.session()
-r = s.get(login_request_url)
-payload['csrf_token'] = re.findall(r'<input type=\"hidden\" name=\"csrf_token\" value=\"(.*?)\">', r.text)[0]
+def main():
+    username = input('Email: ')
+    password = getpass.getpass('Password: ')
 
-r = s.post(login_request_url, data=payload)
-r = s.get(resource)
+    payload = {
+        'from': '/signup',
+        'email': username,
+        'password': password,
+    }
 
-network_id = re.findall(r'var NETWORK = .*?\"id\":\"(.*?)\".*?;', r.text)[0]
-resources = re.findall(r'var RESOURCES = (.*?);', r.text)[0]
+    login_request_url = 'https://piazza.com/class'
+    resource = input('Resource url: ')
 
-documents_url = 'https://piazza.com/class_profile/get_resource/' + network_id + '/'
-documents_json = json.loads(re.findall(r'var RESOURCES = (.*?);', r.text)[0])
-documents_dict = [dict(json) for json in documents_json]
+    s = requests.session()
+    r = s.get(login_request_url)
+    payload['csrf_token'] = re.findall(r'<input type=\"hidden\" name=\"csrf_token\" value=\"(.*?)\">', r.text)[0]
 
-if documents_dict:
-    script_dir = os.path.dirname(__file__)
-    course_name = resource.split('/')[-2]
-    save_dir = os.path.join(script_dir, course_name)
-    os.makedirs(save_dir, exist_ok=True)
+    r = s.post(login_request_url, data=payload)
+    r = s.get(resource)
 
-    for document in documents_dict:
-        if document['config']['resource_type'] == 'file':
-            r = s.get(documents_url + document['id'])
-            try:
-                filename = document['subject']
-                with open(os.path.join(save_dir, filename), 'wb') as f:
-                    f.write(r.content)
-                    print(f'Downloaded {filename}')
-            except Exception:
-                pass
+    network_id = re.findall(r'var NETWORK = .*?\"id\":\"(.*?)\".*?;', r.text)[0]
+    resources = re.findall(r'var RESOURCES = (.*?);', r.text)[0]
 
-        elif document['config']['resource_type'] == 'link':
-            try:
-                filename = document['subject'] + '_url.txt'
-                with open(os.path.join(save_dir, filename), 'w') as f:
-                    f.write(document['content'])
-                    print(f'Url saved into {filename}')
-            except Exception:
-                pass
+    documents_url = 'https://piazza.com/class_profile/get_resource/' + network_id + '/'
+    documents_json = json.loads(re.findall(r'var RESOURCES = (.*?);', r.text)[0])
+    documents_dict = [dict(json) for json in documents_json]
+
+    if documents_dict:
+        script_dir = os.path.dirname(__file__)
+        course_name = resource.split('/')[-2]
+        save_dir = os.path.join(script_dir, course_name)
+        os.makedirs(save_dir, exist_ok=True)
+
+        for document in documents_dict:
+            if document['config']['resource_type'] == 'file':
+                r = s.get(documents_url + document['id'])
+                try:
+                    filename = flatten_string(document['subject'])
+                    filepath = os.path.join(save_dir, filename)
+                    with open(filepath, 'wb') as f:
+                        f.write(r.content)
+                        print(f'Downloaded {filename}')
+                except Exception:
+                    pass
+
+            elif document['config']['resource_type'] == 'link':
+                try:
+                    filename = flatten_string(document['subject']) + '_url.txt'
+                    filepath = os.path.join(save_dir, filename)
+                    with open(filepath, 'w') as f:
+                        f.write(document['subject'] + '\n')
+                        f.write(document['content'] + '\n')
+                        print(f'Url saved into {filename}')
+                except Exception:
+                    pass
+
+
+if __name__ == '__main__':
+    main()
